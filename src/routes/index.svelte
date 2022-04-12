b477c29e5fd80d7c14b580b0b21be180091751f4
echo17666 <3239695085@qq.com>
2022-04-12 21:26:57 +0800

    feat: add icon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>QAQ</h1>
+<img src="../../static/image/favicon.png" alt="sss">
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
