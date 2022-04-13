6f13865bf091888df4d7ccf97a7e91739bf9a796
echo17666 <3239695085@qq.com>
2022-04-13 10:46:14 +0800

    feat: picture show

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,5 @@
+
 <h1>QAQ</h1>
-<img src="../../static/image/favicon.png" alt="sss">
+<button class="btn btn-primary">Button</button>
+<img src="src\image\super2021.jpeg" alt="super2021">
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
