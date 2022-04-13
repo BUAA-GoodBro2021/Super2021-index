65671c4c5fd9ada57ee66d47ed2c888a5a230e8c
echo17666 <3239695085@qq.com>
2022-04-13 11:23:13 +0800

    feat: add card

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,19 @@
 <script>
     import "../app.css"
 </script>
-<h1>QAQ</h1>
-<button class="btn btn-active btn-primary">Button</button>
-<img src="src\image\super2021.jpeg" alt="super2021">
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="navbar bg-neutral text-neutral-content max h-5">
+    <div><img src="src\image\super2021.png" alt="super2021" width="154px"></div>
+  </div>
+
+<div class="flex-center">
+  <div class="card lg:card-side bg-base-100 shadow-xl max-w-6xl">
+    <figure><img src="" alt="echo"></figure>
+    <div class="card-body">
+      <h2 class="card-title">Wudao</h2>
+      <p>Click the button to listen on Spotiwhy app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Listen</button>
+      </div>
+    </div>
+  </div>
+</div>
