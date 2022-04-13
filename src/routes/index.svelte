c48c7bf7feb15b4c664ff70eaf94ffdb05f2c01c
echo17666 <3239695085@qq.com>
2022-04-13 10:50:39 +0800

    feat: reset index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 
 <h1>QAQ</h1>
-<button class="btn btn-primary">Button</button>
 <img src="src\image\super2021.jpeg" alt="super2021">
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
