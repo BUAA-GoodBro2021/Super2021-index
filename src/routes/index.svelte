7a8830864cf386876b4403f5cb2929b4d05f595e
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-04-15 14:30:06 +0800

    feat: change background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -75,15 +75,27 @@
 	<a href="https://github.com/" class="btn">Github</a>
 </div>
 
-<div class="hero min-h-screen" style='background-image: url("src/image/super2021.png");'>
+<div class="hero min-h-screen" style='background-image: url("src/image/bg.jpg");'>
 	<div class="hero-overlay bg-opacity-60" />
-	<div class="hero-content text-center text-neutral-content">
+
+	<div class="grid grid-cols-5 gap-4 max-w-full">
+	<div class="text-left text-neutral-content col-span-3">
 		<div class="max-w-md">
 			<h1 class="mb-5 text-7xl font-bold">Super2021</h1>
-			<p class="mb-5 text-5xl font-bold">超乎你的想象</p>
+			<p class="mb-5 text-5xl font-bold"></p>
 			<h2 class="mb-5 text-6xl font-bold">{now}</h2>
 		</div>
 	</div>
+	<div class="form-control col-span-2 max-width">
+		<div class="input-group">
+		  <input type="text" placeholder="Search…" class="input input-bordered">
+		  <button class="btn btn-square">
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+		  </button>
+		</div>
+	  </div>
+	</div>
+
 </div>
 {#each data as d}
 <div class={d.bgcolor}>
