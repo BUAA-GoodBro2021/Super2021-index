70835fda247a0976bb36f9079c5055b34b711774
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-04-15 12:08:45 +0800

    feat: add individual background and color

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,24 +10,37 @@
 			introduction: 'QAQ',
 			author: 'TualatinX,LLLeo,BITTIO,echo子懿,woarthur',
 			github: 'https://github.com/WuDaoGroup',
-			web: 'https://wudao.netlify.app/'
+			web: 'https://wudao.netlify.app/',
+			background:"https://923259.smushcdn.com/2321054/wp-content/uploads/2019/05/what-is-data-science-1.jpg",
+			bgcolor:"bg-accent"
 		},
 		{
 			projectName: 'StarMap',
 			introduction: 'TAT',
 			author: '几个人&周恩申大哥',
 			github: 'https://github.com/BUAA-OpenMap-Group',
-			web: 'https://www.zhoues.com'
+			web: 'https://www.zhoues.com',
+			background:"https://923259.smushcdn.com/2321054/wp-content/uploads/2019/05/what-is-data-science-1.jpg",
+			bgcolor:"bg-orange-500"
 		}
 	];
+	function completion(a){
+		if(a<10){
+			return '0' + a;
+		}
+		else{
+			return a;
+		}
+	}
 	function showTime() {
-		var date = new Date(); //日期对象
-		day = date.getFullYear() + '年'; //读英文就行了
-		day = day + (date.getMonth() + 1) + '月'; //取月的时候取的是当前月-1如果想取当前月+1就可以了
+		var date = new Date(); 
+		day = date.getFullYear() + '年'; 
+		day = day + (date.getMonth() + 1) + '月'; 
 		day = day + date.getDate() + '日';
-		now = date.getHours() + ':';
-		now = now + date.getMinutes() + ':';
-		now = now + date.getSeconds();
+
+		now = completion(date.getHours()) + ':';
+		now = now + completion(date.getMinutes()) + ':';
+		now = now + completion(date.getSeconds());
 	}
   onMount(() => {
 		console.log('the component has mounted');
@@ -48,18 +61,19 @@
 	<div class="hero-overlay bg-opacity-60" />
 	<div class="hero-content text-center text-neutral-content">
 		<div class="max-w-md">
-			<h1 class="mb-5 text-5xl font-bold">Super2021</h1>
-			<p class="mb-5">超乎你的想象</p>
-			<h2 class="mb-5 text-5xl font-bold">{now}</h2>
+			<h1 class="mb-5 text-7xl font-bold">Super2021</h1>
+			<p class="mb-5 text-5xl font-bold">超乎你的想象</p>
+			<h2 class="mb-5 text-6xl font-bold">{now}</h2>
 		</div>
 	</div>
 </div>
 {#each data as d}
+<div class={d.bgcolor}>
 	<br />
-	<div class="grid grid-cols-12 gap-4">
+	<!-- <div class="grid grid-cols-12 gap-4">
 		<div class="col-span-2" />
 		<div class="card lg:card-side bg-base-100 shadow-xl  col-span-8 ">
-			<!-- <figure class="max-w-6xl"><img src="src\image\super2021.png"  alt="echo"></figure> -->
+			 <figure class="max-w-6xl"><img src="src\image\super2021.png"  alt="echo"></figure> 
 			<div class="card-body">
 				<h2 class="card-title">{d.projectName}</h2>
 				<p>Intro:{d.introduction}</p>
@@ -71,6 +85,23 @@
 				</div>
 			</div>
 		</div>
+	</div> -->
+	<div class="grid grid-cols-12 gap-4">
+		<div class="col-span-2" />
+	<div class="card  bg-base-100 shadow-xl image-full col-span-8">
+		<figure><img src={d.background} alt="Shoes" /></figure>
+		<div class="card-body">
+			<h2 class="card-title mb-5 text-7xl font-bold text-center">{d.projectName}</h2>
+			<p  class="card-title mb-5 text-3xl  text-center">Intro:{d.introduction}</p>
+			<p class="card-title mb-5 text-3xl  text-center">author:</p>
+			<p class="card-title mb-5 text-3xl  text-center">{d.author}</p>
+			<div class="card-actions justify-center">
+				<a href={d.github} class="btn btn-primary">GITHUB</a>
+				<a href={d.web} class="btn btn-primary">WEB</a>
+			</div>
+		</div>
+	  </div>
 	</div>
 	<br />
+</div>
 {/each}
