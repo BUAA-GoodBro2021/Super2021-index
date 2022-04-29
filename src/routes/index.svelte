52d39e8e79569dffb3f128c92c9024e65a6fbf69
echo17666 <72566693+echo17666@users.noreply.github.com>
2022-04-29 14:21:20 +0800

    chore: support enter key to search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -45,6 +45,11 @@
 			return a;
 		}
 	}
+	function submit(event) {
+  		if (event.key == "Enter") {
+    		toSearch();
+  			}
+	}
 	function showTime() {
 		var date = new Date(); 
 		day = date.getFullYear() + '年'; 
@@ -125,7 +130,7 @@
       				</option>
     			{/each}
 			</select>
-		  <input type="search" placeholder="Search…" class="input input-bordered col-span-8" bind:value={search}>
+		  <input type="search" placeholder="Search…" class="input input-bordered col-span-8" bind:value={search} on:keydown={submit}>
 		  
 		  <button class="btn btn-square " on:click|preventDefault={toSearch}>
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
